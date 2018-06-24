bb375f97bae75dd76a2976e4298bd40fa91a6b9e
Carlos Bellino <carlosbellino@gmail.com>
2018-06-24 17:29:56 +0200

    updated imports

--- src/main/java/com/kaname/ticketmanagement/TicketManagementApplication.java
+++ src/main/java/com/kaname/ticketmanagement/TicketManagementApplication.java
@@ -2,7 +2,6 @@ package com.kaname.ticketmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
